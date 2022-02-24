a49678c4353ec7cb53bcc244595192de17215ee0
shadowghost28 <frankwertM2@mail.com>
2022-02-24 05:30:41 -0800

    anadir

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from .views import BlogListView, BlogCreateView
 app_name="blog"
 
 urlpatterns = [
-   path('', BlogListView.as_view(), name="home"),  #siempre esta coma,...
+   path('', BlogListView.as_view(), name="home"),  
    path('create/', BlogCreateView.as_view(), name="create")
 ]
