b8ca41008e5ba588ffe0b8834633d300ceef522f
shadowghost28 <frankwertM2@mail.com>
2022-02-26 04:55:56 -0800

    agregar

--- core/settings.py
+++ core/settings.py
@@ -31,14 +31,15 @@ INSTALLED_APPS = [
      'blog',
 
      'tailwind',
-     'theme',
-
+     'theme'
 ]
 
 TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 
 MIDDLEWARE = [
